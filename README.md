dfa6c84037f0096f1213ff7a8124663ef001253b
foukLor <vitaminotar@yandex.com>
2016-09-30 01:08:35 +0300

    Add function to check who from friends are online

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 8_vk_friends_online
+# 8_vk_friends_online 
+
+
+This script is using to check vk friendlist
+You will see who from friends are online
+
+
+## Usage:
+
+```sh
+python vk_friends_online.py
+```
+
+please use python v3.5
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,16 +1,19 @@
 import vk
+import getpass
+import urllib
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5648299 
 
+def create_connect():
+    print(urllib.request.urlopen("https://vk.com"))
 
 def get_user_login():
-    pass
+    return input("Please, enter your login:")
 
 
 def get_user_password():
-    pass
-
+    return getpass.getpass("Your password:\n")
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
@@ -19,13 +22,21 @@ def get_online_friends(login, password):
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    all_friends = api.friends.get(fields='name', order='name')
+    friends = []
 
+    for friend in all_friends:
+        if friend['online']==1:
+            friends.append(friend['first_name'] + " " + friend['last_name'])
+    return friends
 
 def output_friends_to_console(friends_online):
-    pass
+    if not friends_online:
+        print("Error:no online")
+    print(friends_online)
 
 if __name__ == '__main__':
+    create_connect()
     login = get_user_login()
     password = get_user_password()
     friends_online = get_online_friends(login, password)
