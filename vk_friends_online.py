a9827c0ff25d4bebe3932674cf4a126aded92d0d
foukLor <vitaminotar@yandex.com>
2016-09-30 16:23:25 +0300

    fixed to pep8

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -5,8 +5,10 @@ import urllib
 
 APP_ID = 5648299 
 
+
 def create_connect():
     print(urllib.request.urlopen("https://vk.com"))
+
 
 def get_user_login():
     return input("Please, enter your login:")
@@ -14,6 +16,7 @@ def get_user_login():
 
 def get_user_password():
     return getpass.getpass("Your password:\n")
+
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
@@ -26,14 +29,16 @@ def get_online_friends(login, password):
     friends = []
 
     for friend in all_friends:
-        if friend['online']==1:
+        if friend['online'] == 1:
             friends.append(friend['first_name'] + " " + friend['last_name'])
     return friends
+
 
 def output_friends_to_console(friends_online):
     if not friends_online:
         print("Error:no online")
     print(friends_online)
+
 
 if __name__ == '__main__':
     create_connect()
