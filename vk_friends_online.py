2602ee97e522020d784db73ec367fc65f429208e
foukLor <vitaminotar@yandex.com>
2016-10-02 21:38:17 +0300

    fixed: simplify request to api

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,13 +1,8 @@
 import vk
 import getpass
-import urllib
 
 
 APP_ID = 5648299 
-
-
-def create_connect():
-    print(urllib.request.urlopen("https://vk.com"))
 
 
 def get_user_login():
@@ -23,25 +18,22 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    all_friends = api.friends.get(fields='name', order='name')
-    friends = []
-
-    for friend in all_friends:
-        if friend['online'] == 1:
-            friends.append(friend['first_name'] + " " + friend['last_name'])
+    friends_online_ids = api.friends.getOnline()
+    friends = api.users.get(user_ids=friends_online_ids)
     return friends
 
-
+    
 def output_friends_to_console(friends_online):
     if not friends_online:
         print("Error:no online")
-    print(friends_online)
+    for friend in friends_online:
+        print(friend['first_name'] + " " + friend['last_name'])
 
 
 if __name__ == '__main__':
-    create_connect()
     login = get_user_login()
     password = get_user_password()
     friends_online = get_online_friends(login, password)
